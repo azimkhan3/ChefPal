9846e9ac7e93db7b91391e9cf3f43ce14e1d5e7a
shahirg <shahir13579@gmail.com>
2021-04-20 02:35:25 -0400

    update imports

--- chef_pal/lib/auth/auth_view.dart
+++ chef_pal/lib/auth/auth_view.dart
@@ -1,6 +1,5 @@
 import 'package:chef_pal/auth/firebase_auth.dart';
 import 'package:chef_pal/utils/loading.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
--- chef_pal/lib/auth/firebase_auth.dart
+++ chef_pal/lib/auth/firebase_auth.dart
@@ -1,5 +1,4 @@
 import 'package:chef_pal/ingredient_selection/firestore.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthentificationService {
