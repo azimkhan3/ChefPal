4ec952274e101d7bf2ee689cae7a33af33641c23
shahirg <shahir13579@gmail.com>
2021-04-20 18:27:00 -0400

    split up the search view into seperate widgets

--- chef_pal/lib/recipe_search/search_view.dart
+++ chef_pal/lib/recipe_search/search_view.dart
@@ -1,115 +1,138 @@
 import 'package:chef_pal/recipe_search/recipe_display.dart';
 import 'package:chef_pal/recipe_search/recipe_model.dart';
+import 'package:chef_pal/recipe_search/recipe_preview.dart';
 import 'package:chef_pal/recipe_search/spoonacular_api.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SearchView extends StatefulWidget {
-  @override
-  _SearchViewState createState() => _SearchViewState();
+class SearchResult with ChangeNotifier {
+  List<Recipe> recipes = [];
+  void setRecipes(String search) async {
+    recipes = await ApiService.instance.searchRecipes(search);
+    notifyListeners();
+  }
 }
 
-class _SearchViewState extends State<SearchView> {
-  TextEditingController _searchField = TextEditingController();
-  List<Recipe> _recipes = [];
-  void _searchRecipe() async {
-    List<Recipe> _recipesTemp =
-        await ApiService.instance.searchRecipes(_searchField.text);
-    setState(() {
-      _recipes = _recipesTemp;
-      print('getting recipes');
-    });
-    // print(_recipes);
-  }
-
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
-
+class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 50,
-        ),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width / 1.3,
-                child: TextFormField(
-                  style: TextStyle(color: Colors.grey.shade700),
-                  controller: _searchField,
-                  decoration: InputDecoration(
-                    labelText: "Search all Recipes",
-                    labelStyle: TextStyle(color: Colors.grey),
-                  ),
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width / 1.4,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  color: Colors.white,
-                ),
-                child: MaterialButton(
-                  color: Colors.orange.shade400,
-                  onPressed: () async {
-                    _searchRecipe();
-                  },
-                  child: Text(
-                    "Search",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: SizedBox(
-                  child: ListView.builder(
-                    itemCount: _recipes.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        height: 50,
-                        //color: Colors.amber[colorCodes[index]],
-                        child: MaterialButton(
-                          //color: Colors.orange.shade50,
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                return Dialog(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10)),
-                                  elevation: 16,
-                                  child: Container(
-                                    height: MediaQuery.of(context).size.height /
-                                        1.2,
-                                    width: MediaQuery.of(context).size.height /
-                                        1.1,
-                                    child: RecipeView(_recipes[index]),
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                          child: Text(
-                            '${_recipes[index].title}',
-                            style: TextStyle(
-                              color: Colors.grey.shade900,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ],
+      body: ChangeNotifierProvider<SearchResult>(
+        create: (context) => SearchResult(),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 50,
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: <Widget>[
+                SearchBar(),
+                RecipeSearchDisplay(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class RecipeSearchDisplay extends StatelessWidget {
+  const RecipeSearchDisplay({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<SearchResult>(
+      builder: (_, recipes, __) => Expanded(
+        child: SizedBox(
+          child: ListView.builder(
+            itemCount: recipes.recipes.length,
+            itemBuilder: (BuildContext context, int index) {
+              return RecipePreview(recipe: recipes.recipes[index]);
+              // return Container(
+              //   height: 50,
+              //   //color: Colors.amber[colorCodes[index]],
+              //   child: MaterialButton(
+              //     //color: Colors.orange.shade50,
+              //     onPressed: () {
+              //       showDialog(
+              //         context: context,
+              //         builder: (context) {
+              //           return Dialog(
+              //             shape: RoundedRectangleBorder(
+              //                 borderRadius: BorderRadius.circular(10)),
+              //             elevation: 16,
+              //             child: Container(
+              //               height: MediaQuery.of(context).size.height / 1.2,
+              //               width: MediaQuery.of(context).size.height / 1.1,
+              //               child: RecipeView(recipes.recipes[index]),
+              //             ),
+              //           );
+              //         },
+              //       );
+              //     },
+              //     child: Text(
+              //       '${recipes.recipes[index].title}',
+              //       style: TextStyle(
+              //         color: Colors.grey.shade900,
+              //       ),
+              //     ),
+              //   ),
+              // );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatefulWidget {
+  SearchBar({Key key}) : super(key: key);
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _searchField = TextEditingController();
+    final recipes = Provider.of<SearchResult>(context, listen: false);
+    return Column(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width / 1.3,
+          child: TextFormField(
+            style: TextStyle(color: Colors.grey.shade700),
+            controller: _searchField,
+            decoration: InputDecoration(
+              labelText: "Search all Recipes",
+              labelStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width / 1.4,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            color: Colors.white,
+          ),
+          child: MaterialButton(
+            color: Colors.orange.shade400,
+            onPressed: () async {
+              recipes.setRecipes(_searchField.text);
+            },
+            child: Text(
+              "Search",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
