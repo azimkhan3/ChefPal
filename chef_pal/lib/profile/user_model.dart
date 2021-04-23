ba4ba9b12fb3db08762d0f1eb53a60e6a1e76c40
shahirg <shahir13579@gmail.com>
2021-04-23 05:44:44 -0400

    updated user model with getter

--- chef_pal/lib/profile/user_model.dart
+++ chef_pal/lib/profile/user_model.dart
@@ -4,6 +4,18 @@ class UserData {
   Map<String, dynamic> intolerences;
   UserData({this.uid, this.username, this.diet, this.intolerences});
 
+  String get userName {
+    return this.username;
+  }
+
+  String get userDiet {
+    return this.diet;
+  }
+
+  Map<String, dynamic> get userIntolerences {
+    return this.intolerences;
+  }
+
   Map<String, dynamic> toMap() {
     return {
       'username': username,
