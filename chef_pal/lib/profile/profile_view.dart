346a15576f11afffb5ffeabddab9457e1a4054a1
shahirg <shahir13579@gmail.com>
2021-04-23 05:44:26 -0400

    added intolerences to profile display

--- chef_pal/lib/profile/profile_view.dart
+++ chef_pal/lib/profile/profile_view.dart
@@ -1,4 +1,8 @@
 import 'package:chef_pal/auth/firebase_auth.dart';
+import 'package:chef_pal/ingredient_selection/firestore.dart';
+import 'package:chef_pal/ingredient_selection/ingredient_view.dart';
+import 'package:chef_pal/profile/intolerences_widget.dart';
+import 'package:chef_pal/profile/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,27 +11,182 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userdata = Provider.of<UserData>(context, listen: true);
     return Container(
-      child: Container(
-        alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-          color: Colors.white,
-        ),
-        child: MaterialButton(
-          color: Colors.orange.shade400,
-          onPressed: () {
-            context.read<AuthentificationService>().signOut();
-          },
-          child: Text(
-            "Sign Out",
-            style: TextStyle(
-              color: Colors.white,
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 12),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.width / 4,
+              child: Image.asset(
+                "assets/logo.png",
+                fit: BoxFit.contain,
+              ),
             ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 25,
+                bottom: MediaQuery.of(context).size.height / 20,
+              ),
+              child: Text(
+                "Hello ${userdata.userName}",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 15,
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              child: MaterialButton(
+                color: Colors.orange.shade700,
+                onPressed: () {
+                  context.read<AuthentificationService>().signOut();
+                },
+                child: Text(
+                  "Update Profile",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            IntolerencesWidget(),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width,
+              child: MaterialButton(
+                color: Colors.orange.shade700,
+                onPressed: () {
+                  context.read<AuthentificationService>().signOut();
+                },
+                child: Text(
+                  "Sign Out",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileDropDown extends StatelessWidget {
+  final String category;
+  const ProfileDropDown({Key key, this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final expanded = Provider.of<ExpandedDropDown>(context, listen: false);
+    final ingredients = Provider.of<Map<String, dynamic>>(context);
+
+    List<Widget> dropdownWidgets = [
+      Container(
+        child: GestureDetector(
+          onTap: () {
+            expanded.flip();
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                category,
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 25,
+                ),
+              ),
+              Consumer<ExpandedDropDown>(
+                builder: (_, expanded, __) => expanded.expanded
+                    ? Icon(Icons.arrow_drop_up)
+                    : Icon(Icons.arrow_drop_down),
+              )
+            ],
+          ),
+        ),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 1.0, color: Colors.grey),
           ),
         ),
       ),
+    ];
+    ingredients[category].keys.forEach((ingredient) {
+      dropdownWidgets.add(
+        ProfileDropDownItem(
+          category: category,
+          ingredient: ingredient,
+        ),
+      );
+    });
+
+    return Consumer<ExpandedDropDown>(
+      builder: (_, expanded, __) => Container(
+        width: MediaQuery.of(context).size.width / 1.4,
+        height: expanded.expanded
+            ? MediaQuery.of(context).size.height /
+                30 *
+                (ingredients[category].keys.length + 1)
+            : MediaQuery.of(context).size.height / 30,
+        child: Column(children: dropdownWidgets),
+      ),
+    );
+  }
+}
+
+class ProfileDropDownItem extends StatelessWidget {
+  final String ingredient, category;
+
+  const ProfileDropDownItem(
+      {Key key, @required this.category, @required this.ingredient})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ingredients = Provider.of<Map<String, dynamic>>(context);
+
+    return Consumer<ExpandedDropDown>(
+      builder: (_, expanded, __) => expanded.expanded
+          ? Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 30,
+                width: MediaQuery.of(context).size.width / 1.4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      ingredient,
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width / 30,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        ingredients[category][ingredient] =
+                            !ingredients[category][ingredient];
+                        print(ingredients);
+
+                        context
+                            .read<FirestoreService>()
+                            .updateIngredients(ingredients);
+                      },
+                      child: ingredients[category][ingredient]
+                          ? Icon(Icons.remove)
+                          : Icon(Icons.add),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : Container(),
     );
   }
 }
